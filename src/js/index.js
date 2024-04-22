6b08293a14d6a176d8da6dc26566b7bc18cd01a5
rebecavareiro <rebecavareiro@gmail.com>
2024-04-22 00:52:47 -0300

    pagina web index portfolio, pagina web curriculo e contato, estilos css, defições javascript

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+alert('Oi, Lindo');
+
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
+botaoMostrarProjetos.addEventListener('click', () => {
+    //Adiciona a classe "ativo" nos projetos escondidos
+    mostrarMaisProjetos();
+
+    //Esconde Botao mostrar mais
+    esconderBotao();
+
+})
+
+function esconderBotao() {
+    botaoMostrarProjetos.classList.add("remover");
+}
+
+function mostrarMaisProjetos() {
+    projetosInativos.forEach(projetosInativos => {
+        projetosInativos.classList.add('ativo');
+    });
+}
